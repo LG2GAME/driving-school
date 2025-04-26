d84c961047abc5abdb0903b63556e799caa02918
lg2game <you@example.com>
2025-04-26 22:57:28 +0200

    add icons and images

---
+++ src/assets/icons/icons.ts
@@ -0,0 +1,9 @@
+import carIco from "./auto.svg";
+import indivIco from "./indywidualny.svg";
+import instrIco from "./instruktorzy.svg";
+
+export const icons = {
+  carIco,
+  indivIco,
+  instrIco,
+};
---
+++ src/assets/images/images.ts
@@ -0,0 +1,5 @@
+import drvImg from "./kierowca.png";
+
+export const images = {
+  drvImg,
+};
