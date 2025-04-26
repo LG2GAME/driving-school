27d58cd4c90e30ab3d0c568d03aabcaa604fdb6b
lg2game <you@example.com>
2025-04-26 22:59:41 +0200

    update new exports

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,2 +1,5 @@
 export { default as Navigation } from "./navigation/Navigation";
-export { default as Wrapper } from "./page-wrapper/PageWrapper";
+export { default as Wrapper } from "./wrapper/Wrapper";
+export { default as Container } from "./wrapper/Container";
+export { default as ContentSection } from "./content-section/ContentSection";
+export { default as TextSection } from "./text-section/TextSection";
