bc7438c3239a7c307daac066a193cc3f3d2a69c3
lg2game <you@example.com>
2025-04-30 00:20:15 +0200

    add id props

--- src/components/layout/wrapper/Wrapper.tsx
+++ src/components/layout/wrapper/Wrapper.tsx
@@ -5,10 +5,13 @@ import * as styles from "./wrapper.css";
 type WrapperProps = {
   children: ReactNode;
   direction?: "row" | "column" | "rowRev" | "columnRev";
+  id?: string;
 };
 
-const Wrapper = ({ children, direction = "row" }: WrapperProps) => (
-  <section className={styles.wrapper({ direction })}>{children}</section>
+const Wrapper = ({ children, direction = "row", id }: WrapperProps) => (
+  <section className={styles.wrapper({ direction })} id={id}>
+    {children}
+  </section>
 );
 
 export default Wrapper;
