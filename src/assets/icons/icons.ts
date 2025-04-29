4180b39f24370babf02076133d1548eae728984a
lg2game <you@example.com>
2025-04-30 00:16:37 +0200

    add icons

--- src/assets/icons/icons.ts
+++ src/assets/icons/icons.ts
@@ -1,9 +1,11 @@
 import carIco from "./auto.svg";
 import indivIco from "./indywidualny.svg";
 import instrIco from "./instruktorzy.svg";
+import arrowIco from "./strzalka.svg";
 
 export const icons = {
   carIco,
   indivIco,
   instrIco,
+  arrowIco,
 };
