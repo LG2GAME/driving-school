692e9fb1f3d5f3cc297ae7ee31965726c8bab986
lg2game <you@example.com>
2025-05-01 23:43:27 +0200

    add new images

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,7 +1,11 @@
 import drvImg from "./kierowca.png";
 import beneImg from "./benefity.png";
+import teamImg from "./druzyna.png";
+import instr1Img from "./szarek.png";
 
 export const images = {
   drvImg,
   beneImg,
+  teamImg,
+  instr1Img,
 };
