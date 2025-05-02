5a13e3dbe8a348e44f91fbdbc142821f1b6dec5f
lg2game <you@example.com>
2025-05-02 20:25:31 +0200

    add program and contact section

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,5 @@ export { default as Hero } from "./hero/Hero";
 export { default as Benefits } from "./benefits/Benefits";
 export { default as Courses } from "./courses/Courses";
 export { default as Team } from "./team/Team";
+export { default as Program } from "./program/Program";
+export { default as Contact } from "./contact/Contact";
