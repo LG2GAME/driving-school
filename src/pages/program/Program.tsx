56352b583a6e50edb6b97c4fddec9ee7628e12b1
lg2game <you@example.com>
2025-05-02 20:25:25 +0200

    create Program section

---
+++ src/pages/program/Program.tsx
@@ -0,0 +1,45 @@
+import { ContentWrapper, Wrapper } from "@components/layout";
+import { Card, TextBlock } from "@components/ui";
+
+import { useMediaQuery } from "@hooks/useMediaQuery";
+
+import { programData } from "@assets/data";
+import { mq } from "@styles/theme.css";
+
+interface ProgramStepInterface {
+  id: number;
+  icon: string;
+  title: string;
+  description: string;
+}
+
+const Program = () => {
+  const isDesktop: boolean = !useMediaQuery(mq.lg);
+
+  return (
+    <Wrapper direction="column">
+      <ContentWrapper>
+        <TextBlock
+          header="Plan szkoleniowy"
+          description="Każdy krok przybliży Cię do sukcesu za kierownicą – zobacz, jak wygląda Twój kurs."
+        />
+      </ContentWrapper>
+      <ContentWrapper direction={isDesktop ? "rowEnd" : "column"}>
+        {programData.map(
+          ({ id, icon, title, description }: ProgramStepInterface) => (
+            <Card
+              variant="centered"
+              stepSpacing={isDesktop ? (id - 1) * 45 : undefined}
+            >
+              <img src={icon} alt="" />
+              <h3>{title}</h3>
+              <p>{description}</p>
+            </Card>
+          )
+        )}
+      </ContentWrapper>
+    </Wrapper>
+  );
+};
+
+export default Program;
