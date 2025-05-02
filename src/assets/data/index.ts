a673eba54d81a819572a82f628c3bd038d13f6ef
lg2game <you@example.com>
2025-05-02 20:24:25 +0200

    add datas

--- src/assets/data/index.ts
+++ src/assets/data/index.ts
@@ -1,3 +1,4 @@
 export { benefitsData } from "./benefitsData";
 export { coursesData } from "./coursesData";
 export { instructorsData } from "./instructorsData";
+export { programData } from "./programData";
---
+++ src/assets/data/programData.ts
@@ -0,0 +1,30 @@
+import { icons } from "@assets/icons/icons";
+
+export const programData = [
+  {
+    id: 1,
+    icon: icons.learnIco,
+    title: "Krok 1.",
+    description:
+      "Poznaj przepisy drogowe i zasady bezpiecznej jazdy na zajęciach teoretycznych.",
+  },
+  {
+    id: 2,
+    icon: icons.roadIco,
+    title: "Krok 2.",
+    description:
+      "Doskonal umiejętności za kierownicą z pomocą doświadczonych instruktorów.",
+  },
+  {
+    id: 3,
+    icon: icons.knwlIco,
+    title: "Krok 3.",
+    description: "Sprawdź swoją wiedzę i umiejętności przed egzaminem.",
+  },
+  {
+    id: 4,
+    icon: icons.examlIco,
+    title: "Krok 4.",
+    description: "Przystąp do egzaminu z pewnością i pełnym przygotowaniem.",
+  },
+];
