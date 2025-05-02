de411aad1fbdeef19688842ce1992047dc84cc7b
lg2game <you@example.com>
2025-05-02 20:24:36 +0200

    add icons

--- src/assets/icons/icons.ts
+++ src/assets/icons/icons.ts
@@ -1,11 +1,23 @@
+import arrowIco from "./strzalka.svg";
 import carIco from "./auto.svg";
+import examlIco from "./egzamin.svg";
 import indivIco from "./indywidualny.svg";
 import instrIco from "./instruktorzy.svg";
-import arrowIco from "./strzalka.svg";
+import knwlIco from "./wiedza.svg";
+import learnIco from "./nauka.svg";
+import roadIco from "./droga.svg";
+import telIco from "./telefon.svg";
+import txtIco from "./chat.svg";
 
 export const icons = {
+  arrowIco,
   carIco,
+  examlIco,
   indivIco,
   instrIco,
-  arrowIco,
+  knwlIco,
+  learnIco,
+  roadIco,
+  telIco,
+  txtIco,
 };
